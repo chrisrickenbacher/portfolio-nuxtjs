751f1cdff02ce959a30a194da3ff3082c511e888
Christian Rickenbacher <christian.rickenbacher@gmail.com>
2022-01-25 12:07:17 +0100

    move js script to vue file

--- pages/index.vue
+++ pages/index.vue
@@ -64,6 +64,31 @@ export default {
   return {
     projects
     }
+  },
+
+  methods: {
+    variableWidthAxis(column, container) {
+        const maxWidthAxis = 200;
+        const minWidthAxis = 50;
+        const maxColumnWidth = 600;
+        const minColumnWidth = 375;
+
+        const width = column.clientWidth;
+
+        const percent = (width - minColumnWidth) / (maxColumnWidth - minColumnWidth);
+        const scale = percent * (maxWidthAxis - minWidthAxis) + minWidthAxis;
+
+        const newWidth = width > maxColumnWidth ? maxWidthAxis : width < minColumnWidth ? minWidthAxis : scale;
+
+        container.style.setProperty("--width-axis", newWidth.toString());
+    }
+  },
+  mounted() {
+    var column = document.getElementById("column");
+    var container = document.querySelector("body");
+    this.variableWidthAxis(column, container)
+    window.addEventListener("resize",  this.variableWidthAxis(column, container));
+    
   }
 }
 </script>
